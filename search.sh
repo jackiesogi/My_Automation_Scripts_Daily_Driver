d2870ef3c2865b86209ccbe7f06d2fff0a2a72df
jackiesogi <jackiesogi@gmail.com>
2024-07-22 04:40:53 +0800

    Add help page and program metadata

--- search.sh
+++ search.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
+PROGRAM="search.sh"
+DESCRIPTION="A lightweight script to find keyword or macros in project"
+VERSION="v1.1"
+AUTHOR="Jackie Chen"
+
 SEARCH_PATH=('/usr/include')
 SEARCH_MACRO=""
 SEAECH_MACRO_ONLY=1
 
+function print_usage() {
+	echo "$PROGRAM $VERSION -- $DESCRIPTION" 
+	echo ""
+	echo 'Usage: search.sh [-d] ${DIRECTORY} [-m] {on,off} [-h]'
+	echo "    -d \${DIRECTORY}    for example, -d ~/.local/scripts"
+	echo "    -m {on,off}        for example, -m off for search keyword"
+	echo "    -h                 show help page"
+}
+
 while [[ $# -gt 0 ]]; do
 	case $1 in
+		-h)
+			print_usage
+			exit 0
+			;;
 		-d)
 			SEARCH_PATH=("$2")
 			shift 2
